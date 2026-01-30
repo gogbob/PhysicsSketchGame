cfded01d8b090be08455f46b97dd466aa0ace9f7
gogbob <christophelewis@videotron.ca>
2026-01-30 13:12:26 -0500

    Created Code Structure

--- core/src/main/java/io/github/physics_game/Main.java
+++ core/src/main/java/io/github/physics_game/Main.java
@@ -1,32 +1,61 @@
 package io.github.physics_game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
+    FitViewport viewport;
     private Texture image;
+    private Texture ballImage;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
         image = new Texture("libgdx.png");
+        ballImage = new Texture("simple_ball.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        input();
+        logic();
+        draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         image.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+        super.resize(width, height);
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+        batch.draw(ballImage, 0, 0, 1, 1);
+        batch.end();
     }
 }
